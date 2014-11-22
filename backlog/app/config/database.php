efde9a96ac41b0131821a5544f89ca339c2f2d07
masbaty <masbaty@gmail.com>
2014-11-22 17:33:08 -0500

    Got database working

--- backlog/app/config/database.php
+++ backlog/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'backlog',
+			'username'  => 'root',
+			'password'  => 'AoFKXYMlGN',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
