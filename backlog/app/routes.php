4b540d8ec192060c6e56ecf59da9256d0c4b145f
masbaty <masbaty@gmail.com>
2014-11-23 19:44:11 -0500

    Experimenting with migrations

--- backlog/app/routes.php
+++ backlog/app/routes.php
@@ -17,7 +17,47 @@ Route::get('/', function()
 });
 
 
+Route::get('/practice-creating', function() {
+    $game = new Game();
 
+    $game->title = 'The Legend of Zelda: Wind Waker';
+    $game->franchise = 'The Legend of Zelda';
+    $game->genre = 'Action Adventure';
+    $game->platform = 'Nintendo GameCube';
+    $game->status = 'In Progress';
+    $game->progress = "Just acquired Earth God's Lyric";
+    $game->currently_playing = 'Y';
+    $game->rating = '5';
+    $game->notes = 'I also own the HD version on the Wii U';
+
+    $game->save();
+
+    return 'Your entry has been saved.';
+});
+
+Route::get('/practice-reading', function() {
+    $games = Game::all();
+
+    if($games->isEmpty() != TRUE) {
+        foreach($games as $game) {
+            echo $game->title.'<br />';
+        }
+    }
+    else {
+        return 'Nothing found.';
+    }
+});
+
+Route::get('/practice-reading-one', function() {
+    $game = Game::where('currently_playing', 'LIKE', '%Y%')->first();
+
+    if($game) {
+        return $game->title;
+    }   
+    else {
+        return 'Nothing found.';
+    }
+});
 
 
 // Debugging Routes ///////////////////////////
