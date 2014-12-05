0ede8cb8d955b03823b95ae40626e9d80d45c79a
masbaty <masbaty@gmail.com>
2014-12-04 19:05:18 -0500

    Created homepage

---
+++ backlog/app/views/_master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>@yield("title","Project 3")</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" 
+		href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" 
+		type="text/css" />
+	<link rel="stylesheet" href="/css/style.css" type="text/css" />
+
+	@yield("head")
+
+
+</head>
+<body>
+
+	@yield("content")
+
+</body>
+</html>
---
+++ backlog/app/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends("_master")
+
+@section("title")
+	Project 4 - Final Project
+@stop
+
+@section("head")
+@stop
+
+@section("content")
+
+	<h1>Final Project</h1>
+
+	
+
+@stop
